8ba1ca0c162fe11fe570630b95b6d61524a38135
nikola <nikola.janicijevic@unitn.it>
2016-06-09 20:30:20 +0200

    This is the code that is used for the test of the connectivity during the building of the testbed. This code is RX part and on display it has only number of the received packets in order to calculate later the PDR

---
+++ main.c
@@ -0,0 +1,141 @@
+/*! ----------------------------------------------------------------------------
+ *  @file    main.c
+ *  @brief   Simple RX example code
+ *
+ * @attention
+ *
+ * Copyright 2015 (c) DecaWave Ltd, Dublin, Ireland.
+ *
+ * All rights reserved.
+ *
+ * @author DecaWave
+ */
+#include "deca_device_api.h"
+#include "deca_regs.h"
+#include "lcd.h"
+#include "port.h"
+
+/* Example application name and version to display on LCD screen. */
+#define APP_NAME "SIMPLE RX v1.1 TX"
+
+/* Default communication configuration. We use here EVK1000's default mode (mode 3). */
+static dwt_config_t config = {
+    2,               /* Channel number. */
+    DWT_PRF_16M,     /* Pulse repetition frequency. */
+    DWT_PLEN_64,   /* Preamble length. */
+    DWT_PAC8,       /* Preamble acquisition chunk size. Used in RX only. */
+    3,               /* TX preamble code. Used in TX only. */
+    3,               /* RX preamble code. Used in RX only. */
+    1,               /* Use non-standard SFD (Boolean) */
+    DWT_BR_6M8,     /* Data rate. */
+    DWT_PHRMODE_STD, /* PHY header mode. */
+    (65 + 16 - 8) /* SFD timeout (preamble length + 1 + SFD length - PAC size). Used in RX only. */
+};
+
+/* Buffer to store received frame. See NOTE 1 below. */
+#define FRAME_LEN_MAX 127
+static uint8 rx_buffer[FRAME_LEN_MAX];
+
+/* Hold copy of status register state here for reference, so reader can examine it at a breakpoint. */
+static uint32 status_reg = 0;
+
+/* Hold copy of frame length of frame received (if good), so reader can examine it at a breakpoint. */
+static uint16 frame_len = 0;
+	uint32 powerValue, powerValue2=0;
+#define LCD_BUFF_LEN (80)
+uint8 dataseq[LCD_BUFF_LEN];
+uint8 dataseq1[LCD_BUFF_LEN];
+int received = 0;
+
+/**
+ * Application entry point.
+ */
+int main(void)
+{
+    /* Start with board specific hardware init. */
+    peripherals_init();
+
+    /* Display application name on LCD. */
+    lcd_display_str(APP_NAME);
+
+    /* Reset and initialise DW1000.
+     * For initialisation, DW1000 clocks must be temporarily set to crystal speed. After initialisation SPI rate can be increased for optimum
+     * performance. */
+    reset_DW1000(); /* Target specific drive of RSTn line into DW1000 low for a period. */
+    spi_set_rate_low();
+    dwt_initialise(DWT_LOADNONE);
+    spi_set_rate_high();
+
+    powerValue = dwt_read32bitreg(RX_SNIFF_ID);
+    dwt_write32bitreg(RX_SNIFF_ID,0xdead0002);
+    powerValue2=(dwt_read32bitreg(RX_SNIFF_ID) && RX_SNIFF_SNIFF_ONT_MASK);
+    /* Configure DW1000. */
+    dwt_configure(&config);
+
+    /* Loop forever receiving frames. */
+    while (1)
+    {
+        int i;
+
+
+        /* Clear local RX buffer to avoid having leftovers from previous receptions  This is not necessary but is included here to aid reading
+         * the RX buffer.
+         * This is a good place to put a breakpoint. Here (after first time through the loop) the local status register will be set for last event
+         * and if a good receive has happened the data buffer will have the data in it, and frame_len will be set to the length of the RX frame. */
+        for (i = 0 ; i < FRAME_LEN_MAX; i++ )
+        {
+            rx_buffer[i] = 0;
+        }
+
+        /* Activate reception immediately. See NOTE 2 below. */
+        dwt_rxenable(0);
+
+        /* Poll until a frame is properly received or an error/timeout occurs. See NOTE 3 below.
+         * STATUS register is 5 bytes long but, as the event we are looking at is in the first byte of the register, we can use this simplest API
+         * function to access it. */
+        while (!((status_reg = dwt_read32bitreg(SYS_STATUS_ID)) & (SYS_STATUS_RXFCG | SYS_STATUS_ALL_RX_ERR)))
+        { };
+
+        if (status_reg & SYS_STATUS_RXFCG)
+        {
+            /* A frame has been received, copy it to our local buffer. */
+            frame_len = dwt_read32bitreg(RX_FINFO_ID) & RX_FINFO_RXFL_MASK_1023;
+            if (frame_len <= FRAME_LEN_MAX)
+            {
+                dwt_readrxdata(rx_buffer, frame_len, 0);
+            }
+
+            /* Clear good RX frame event in the DW1000 status register. */
+            dwt_write32bitreg(SYS_STATUS_ID, SYS_STATUS_RXFCG);
+        }
+        else
+        {
+            /* Clear RX error events in the DW1000 status register. */
+            dwt_write32bitreg(SYS_STATUS_ID, SYS_STATUS_ALL_RX_ERR);
+        }
+
+        received++;
+        writetoLCD( 1, 0,  0x2);
+
+    	sprintf((char*)&dataseq[0], "DecaRanging  %02u", received);
+    	writetoLCD( 40, 1, dataseq); //send some data
+
+    	sprintf((char*)&dataseq1[0], "                 ");
+    	writetoLCD( 16, 1, dataseq1); //send some data
+
+
+    }
+}
+
+/*****************************************************************************************************************************************************
+ * NOTES:
+ *
+ * 1. In this example, maximum frame length is set to 127 bytes which is 802.15.4 UWB standard maximum frame length. DW1000 supports an extended
+ *    frame length (up to 1023 bytes long) mode which is not used in this example.
+ * 2. Manual reception activation is performed here but DW1000 offers several features that can be used to handle more complex scenarios or to
+ *    optimise system's overall performance (e.g. timeout after a given time, automatic re-enabling of reception in case of errors, etc.).
+ * 3. We use polled mode of operation here to keep the example as simple as possible but RXFCG and error/timeout status events can be used to generate
+ *    interrupts. Please refer to DW1000 User Manual for more details on "interrupts".
+ * 4. The user is referred to DecaRanging ARM application (distributed with EVK1000 product) for additional practical example of usage, and to the
+ *    DW1000 API Guide for more details on the DW1000 driver functions.
+ ****************************************************************************************************************************************************/
